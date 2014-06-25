f5ed8eb3b6d8c66da30cf8c9eed84726c196d176
Mouther <za7788az@hotmail.com>
2014-06-26 03:06:48 +0800

    finish

--- TocHW4.java
+++ TocHW4.java
@@ -10,14 +10,14 @@ public class TocHW4
 {
     public static String read(String urlStr) throws IOException 
     {  
-        //URL url = new URL(urlStr);
-        //URLConnection connection = url.openConnection();
-        //connection.setDoInput(true);
-        //InputStream inStream = connection.getInputStream();
+        URL url = new URL(urlStr);
+        URLConnection connection = url.openConnection();
+        connection.setDoInput(true);
+        InputStream inStream = connection.getInputStream();
         
-		BufferedReader in = new BufferedReader(new FileReader("5385b69de7259bb37d925971"));
+		//BufferedReader in = new BufferedReader(new FileReader("5385b69de7259bb37d925971"));
         
-		//BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
+		BufferedReader in = new BufferedReader(new InputStreamReader(inStream));
         String s;  
         StringBuilder sb = new StringBuilder();  
 
@@ -31,7 +31,28 @@ public class TocHW4
         return sb.toString();  
     }  
 
-    public static void main(String args[]) throws IOException
+ 	public static void show_max_min(JSONArray ary, String str)
+	{
+		int max = -1, min = Integer.MAX_VALUE;
+		
+		for(int i = 0; i < ary.length(); i++)
+		{
+			String address = ary.getJSONObject(i).getString("土地區段位置或建物區門牌"); 
+
+			if(address.indexOf(str) != -1)
+			{
+				if(ary.getJSONObject(i).getInt("總價元") > max)
+					max = ary.getJSONObject(i).getInt("總價元");
+
+				if(ary.getJSONObject(i).getInt("總價元") < min)
+					min = ary.getJSONObject(i).getInt("總價元");
+			}
+		}
+
+		System.out.print("最高成交價:" + max + ", 最低成交價:" + min);
+	}
+
+	public static void main(String args[]) throws IOException
     {
         JSONArray ary = new JSONArray(read(args[0]));
 		
@@ -89,18 +110,22 @@ public class TocHW4
 			}
 		}
 
-		int longth = -1, i;
+		int longth = -1;
 
-		for(i = 0; i < add_buf.size(); i++)
+		for(int i = 0; i < add_buf.size(); i++)
 		{
 			if(month.get(i).length() > longth)
 				longth = month.get(i).length();
 		}
 
-		for(i = 0; i < add_buf.size(); i++)
+		for(int i = 0; i < add_buf.size(); i++)
 		{
 			if(month.get(i).length() == longth)
-				System.out.println(add_buf.get(i));
+			{
+				System.out.print(add_buf.get(i) + ", ");
+				show_max_min(ary, add_buf.get(i));
+				System.out.println();
+			}
 			//System.out.println(add_buf.get(i) + " !! " + month.get(i));
 		}
 			
